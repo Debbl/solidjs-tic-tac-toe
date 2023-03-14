71c8cb36b5b9370a8c59252df31dca28ff710256
Debbl <me@aiwan.run>
2023-03-14 11:00:43 +0800

    refactor

--- src/composable/GamePlay.ts
+++ src/composable/GamePlay.ts
@@ -1,22 +1,33 @@
-import { Setter, useTransition } from "solid-js";
+import { Accessor, createSignal, Setter } from "solid-js";
 import { Board } from "../types";
-import EventBus from "./EventBus";
+import _ from "lodash";
 
 const INIT_BOARD: Board = Array.from({ length: 9 }).map(() => "");
 
-const [_, start] = useTransition();
+export default class GamePlay {
+  private _board: Accessor<Board>;
+  private _setBoard: Setter<Board>;
 
-type Events = { update: Board };
-class Game extends EventBus<Events> {
-  board: Board;
-  constructor(board: Board) {
-    super();
-    this.board = board;
+  get board() {
+    return this._board();
   }
+
+  constructor() {
+    [this._board, this._setBoard] = createSignal(_.cloneDeep(INIT_BOARD), {
+      equals: false,
+    });
+  }
+
   protected setBoard(board: Board) {
-    const newBoard = [...board];
-    this.board = newBoard;
-    this.emit("update", newBoard);
+    this._setBoard(board);
+  }
+  protected checkLine(x: number, y: number, z: number) {
+    const { board } = this;
+    return Math.abs(+board[x] + +board[y] + +board[z]) === 3;
+  }
+  protected checkComplete() {
+    const { board } = this;
+    return board.every((i) => i !== "");
   }
   protected getMinIncome(board: Board, isAI: boolean) {
     const res = this.checkStatus();
@@ -48,11 +59,31 @@ class Game extends EventBus<Events> {
       return baseScore;
     }
   }
-  protected checkLine(x: number, y: number, z: number) {
+  private getAIPlayerNext() {
     const { board } = this;
-    return Math.abs(+board[x] + +board[y] + +board[z]) === 3;
+    let baseScore = -Infinity;
+    let next: number | undefined;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === "") {
+        board[i] = "1";
+        const score = this.getMinIncome(board, false);
+        board[i] = "";
+        if (score > baseScore) {
+          baseScore = score;
+          next = i;
+        }
+      }
+    }
+    return next;
   }
-  checkStatus() {
+  private AIPlayer() {
+    const next = this.getAIPlayerNext();
+    if (next === undefined) return;
+    this.board[next] = "1";
+    this.setBoard(this.board);
+  }
+
+  public checkStatus() {
     for (let i = 0; i < 9; i += 3) {
       if (this.checkLine(i, i + 1, i + 2)) return this.board[i];
     }
@@ -65,47 +96,15 @@ class Game extends EventBus<Events> {
     if (this.checkComplete()) return "0";
     return "";
   }
-  protected checkComplete() {
-    const { board } = this;
-    return board.every((i) => i !== "");
-  }
-  getAIPlayerNext() {
-    const { board } = this;
-    let baseScore = -Infinity;
-    let next: number | undefined;
-    for (let i = 0; i < board.length; i++) {
-      if (board[i] === "") {
-        board[i] = "1";
-        const score = this.getMinIncome(board, false);
-        console.log("score", score);
-        board[i] = "";
-        if (score > baseScore) {
-          baseScore = score;
-          next = i;
-        }
-      }
-    }
-    return next;
-  }
-  AIPlayer = () =>
-    start(() => {
-      const next = this.getAIPlayerNext();
-      console.log(next);
-      if (!next) return;
-      this.board[next] = "1";
-      this.setBoard(this.board);
-    });
-  handleClick = (item: string, index: number) => {
+  public handleClick(item: string, index: number) {
     const { board } = this;
     if (item === "") {
       board[index] = "-1";
-      this.AIPlayer();
       this.setBoard(board);
+      this.AIPlayer();
     }
-  };
-  resetGame = () => {
-    this.setBoard(INIT_BOARD);
-  };
+  }
+  public resetGame() {
+    this.setBoard(_.cloneDeep(INIT_BOARD));
+  }
 }
-
-export default Game;
--- src/utils/AIPlayer.ts
+++
@@ -1,2 +0,0 @@
-class AIPlayer {}
-export default AIPlayer;
